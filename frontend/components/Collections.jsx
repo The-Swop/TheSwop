5075a791ba3743e774bb78027ea4cff78627017c
cp-daniel-mccoy <86984714+cp-daniel-mccoy@users.noreply.github.com>
2022-10-13 17:48:05 -0400

    Fixed collections section for desktop/tablet

--- frontend/components/Collections.jsx
+++ frontend/components/Collections.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react"
 import swop64 from "../assets/swop-64x64.png";
 import goatedGamer from "../assets/icons/goated-gamer.png";
+import astroSamurai from "../assets/icons/astro-samurai.png"
+import astroAnime from "../assets/icons/astro-anime.png";
 
 export function Collections() {
 
@@ -15,25 +17,25 @@ export function Collections() {
       <div className='myCollections'>
       <div className='collectionCard'>
         <img src={goatedGamer} alt='collectionLogo' />
-          <div className='collectionButtons'>
-            <button>Entrepot</button>
-            <button>Website</button>
-          </div>
+        <h1>Goated Gamer DAO</h1>
+        <div className='collectionButtons'>
+          <button onClick={() => { window.location = "https://entrepot.app/marketplace/goated-gamer-dao"; }}>Go To Marketplace</button>
         </div>
-        <div className='collectionCard' onClick={redirectToSwop}>
-        <img src={swop64} alt='collectionLogo' />
-          <div className='collectionButtons'>
-            <button>Entrepot</button>
-            <button>Website</button>
-          </div>
+      </div>
+      <div className='collectionCard'>
+        <img src={astroSamurai} alt='collectionLogo' />
+        <h1>Astro Samurai</h1>
+        <div className='collectionButtons'>
+          <button onClick={() => { window.location = "https://entrepot.app/marketplace/astro-samurai"; }}>Go To Marketplace</button>
         </div>
-        <div className='collectionCard' onClick={redirectToSwop}>
-        <img src={swop64} alt='collectionLogo' />
-          <div className='collectionButtons'>
-            <button>Entrepot</button>
-            <button>Website</button>
-          </div>
+      </div>
+      <div className='collectionCard'>
+        <img src={astroAnime} alt='collectionLogo' />
+        <h1>Astro Anime</h1>
+        <div className='collectionButtons'>
+          <button onClick={() => { window.location = "https://entrepot.app/marketplace/goated-gamer-dao"; }}>Go To Marketplace</button>
         </div>
+      </div>
       </div>
     </div>
   )
