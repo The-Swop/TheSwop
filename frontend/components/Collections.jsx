bd5450cbc5c2228af9a645a73745df5c103c6545
cp-daniel-mccoy <86984714+cp-daniel-mccoy@users.noreply.github.com>
2022-10-13 03:16:21 -0400

    Moved collections section into it's own component

---
+++ frontend/components/Collections.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react"
+import swop64 from "../assets/swop-64x64.png";
+
+export function Collections() {
+
+  const redirectToSwop = async () => {
+    window.location.replace("https://twitter.com/the_swop");
+  }
+
+  return (
+    <div className='collectionsContainer'>
+      <h1>Our Collections</h1>
+      <div className='separator' />
+      <div className='myCollections'>
+      <div className='collectionCard' onClick={redirectToSwop}>
+        <img src={swop64} alt='collectionLogo' />
+          <div className='collectionButtons'>
+            <button>Entrepot</button>
+            <button>Website</button>
+          </div>
+        </div>
+        <div className='collectionCard' onClick={redirectToSwop}>
+        <img src={swop64} alt='collectionLogo' />
+          <div className='collectionButtons'>
+            <button>Entrepot</button>
+            <button>Website</button>
+          </div>
+        </div>
+        <div className='collectionCard' onClick={redirectToSwop}>
+        <img src={swop64} alt='collectionLogo' />
+          <div className='collectionButtons'>
+            <button>Entrepot</button>
+            <button>Website</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- frontend/components/MainSite.jsx
+++ frontend/components/MainSite.jsx
@@ -1,15 +1,9 @@
 import React, { useEffect, useState } from "react"
-import { PlugWallet } from "../utils/PlugWallet";
 import websiteHeader from "../assets/website-header.png";
-import swop64 from "../assets/swop-64x64.png";
 import { NavBar } from "./NavBar";
+import { Collections } from "./Collections";
 
 export function MainSite() {
-
-  const redirectToSwop = async () => {
-    window.location.replace("https://twitter.com/the_swop");
-  }
-
 
   return (
     <div className='main'>
@@ -20,33 +14,7 @@ export function MainSite() {
         <h1>Welcome To</h1>
         <img src={websiteHeader} />
       </div>
-      <div className='collectionsContainer'>
-        <h1>Our Collections</h1>
-        <div className='separator' />
-        <div className='myCollections'>
-        <div className='collectionCard' onClick={redirectToSwop}>
-            <img src={swop64} alt='collectionLogo' />
-            <div className='collectionButtons'>
-              <button>Entrepot</button>
-              <button>Website</button>
-            </div>
-          </div>
-          <div className='collectionCard' onClick={redirectToSwop}>
-          <img src={swop64} alt='collectionLogo' />
-            <div className='collectionButtons'>
-              <button>Entrepot</button>
-              <button>Website</button>
-            </div>
-          </div>
-          <div className='collectionCard' onClick={redirectToSwop}>
-          <img src={swop64} alt='collectionLogo' />
-            <div className='collectionButtons'>
-              <button>Entrepot</button>
-              <button>Website</button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Collections />
     </div>
   )
 }
