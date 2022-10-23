9982988d3f2ec7b210c98f24d1eb0435368b45ad
cp-daniel-mccoy <86984714+cp-daniel-mccoy@users.noreply.github.com>
2022-10-23 03:42:58 -0400

    Fixed partners section, needs mobile flow

--- frontend/components/Partners.jsx
+++ frontend/components/Partners.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react"
-import swop64 from "../assets/swop-64x64.png";
 import hyperX from "../assets/icons/hyperx.png";
 import razer from "../assets/icons/razer.png";
+import dscvr from "../assets/icons/dscvr-logo.png";
+import plethora from "../assets/icons/plethora.png";
+import cyman from "../assets/icons/cyman.png";
+import puzzles from "../assets/icons/puzzles.png";
+import dragonz from "../assets/icons/dragonz.png";
 
 export function Partners() {
 
@@ -10,16 +14,15 @@ export function Partners() {
       <h1>Partners</h1>
       <div className='separator' />
       <div className='majorPartnersSection'>
-        <img src={hyperX} />
-        <img src={razer} />
-        <img src={swop64} />
+        <img onClick={() => { window.location = "https://hx.hyperx.gg/qjg5Ycmb"; }}src={hyperX} />
+        <img onClick={() => { window.location = "https://www.razer.com/"; }}src={razer} />
+        <img onClick={() => { window.location = "https://dscvr.one/p/the-swop"; }}src={dscvr} />
       </div>
       <div className='minorPartnersSection'>
-        <img src={swop64} />
-        <img src={swop64} />
-        <img src={swop64} />
-        <img src={swop64} />
-        <img src={swop64} />
+        <img onClick={() => { window.location = "https://entrepot.app/marketplace/cyman"; }}src={cyman} />
+        <img onClick={() => { window.location = "https://5sdab-uiaaa-aaaal-aalgq-cai.ic0.app/"; }}src={puzzles} />
+        <img onClick={() => { window.location = "https://plethora.game/"; }}src={plethora} />
+        <img onClick={() => { window.location = "https://nyph3-wyaaa-aaaap-aaaaa-cai.ic0.app/"; }}src={dragonz} />
       </div>
     </div>
   )
